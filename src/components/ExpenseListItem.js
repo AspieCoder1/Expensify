30117c77734a6fd5259eb1a7225ed451425a7eec
sai109 <lukebraithwaite7@gmail.com>
2018-04-04 19:02:41 +0100

    private route set up

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,16 +4,12 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
-    </Link>
-    <p>
-      {numeral(amount).format('$0,0.00')}
-      -
-      {moment(createdAt).format('MMMM Do, YYYY')}
-    </p>
-  </div>
+	<div>
+		<Link to={`/edit/${id}`}>
+			<h3>{description}</h3>
+		</Link>
+		<p>{numeral(amount).format('$0,0.00')} - {moment(createdAt).format('Do MMM, YYYY')}</p>
+	</div>
 );
 
 export default ExpenseListItem;
